47efa2406eb12a8f7bef7051cfad7f4ae10c80c4
holodnsk <holodnsk@gmail.com>
2022-12-11 18:36:40 +0700

    learn gen1 models

--- JFDL.py
+++ JFDL.py
@@ -7,61 +7,60 @@ import zipfile
 
 def getwinHeadTarget():
  df_full = pd.read_csv("targethead.csv", sep=',')
- return df_full
+ return df_full.astype(float)
 
 def getwinMiddleTarget():
- df_full = pd.read_csv("winMiddle.csv", sep=',')
- return df_full
+ df_full = pd.read_csv("targetmiddle.csv", sep=',')
+ return df_full.astype(float)
 
 def getwinTailTarget():
- df_full = pd.read_csv("winTail.csv", sep=',')
- return df_full
+ df_full = pd.read_csv("targettail.csv", sep=',')
+ return df_full.astype(float)
 
-def getFeatures():
- df_full = pd.read_csv("feature.csv", sep=',')
- return df_full
+def getFeaturesTail():
+ df_full = pd.read_csv("featureTail.csv", sep=',',dtype="bool")
+ return df_full.astype(bool)
+
+def getFeaturesMiddle():
+ df_full = pd.read_csv("featureMiddle.csv", sep=',',dtype="bool")
+ return df_full.astype(bool)
+
+def getFeaturesHead():
+ df_full = pd.read_csv("featureHead.csv", sep=',',dtype="bool")
+ return df_full.astype(bool)
 
 
-features = getFeatures()
-targetHead = getwinHeadTarget()
-# targetMiddle = getwinMiddleTarget()
-# targetTail = getwinTailTarget()
+def modelTrain(X,Y):
+ model = Sequential()
+ model.add(Dense(165, input_dim=165, activation='relu'))
+ model.add(Dense(428, activation='relu'))
+ model.add(Dense(28, activation='relu'))
+ model.add(Dense(1, activation='tanh'))  # !!!!! логичней tanh так так есть положительные и отрицательные значения
+ opt = tf.keras.optimizers.Adam(learning_rate=1e-5)
+ model.compile(loss='mse', optimizer=opt, metrics=['mae'])
+ model.fit(X, Y, batch_size=512, epochs=20, validation_split=0.2, verbose=2)
+ return model
 
-X = features.astype(int)  # Присваиваем им тип данных - int
-YHead = targetHead.astype(float)
-# YMiddle = targetMiddle.astype(float)
-# YTail = targetTail.astype(float)
-
-print(X.shape)  # Выводим размерность X
-print(YHead.shape)  # Выводим размерность Y
-# print(YMiddle.shape)  # Выводим размерность Y
-# print(YTail.shape)  # Выводим размерность Y
-
-model = Sequential()
-model.add(Dense(165, input_dim=165, activation='relu'))
-# model.add(Dropout(0.5))
-model.add(Dense(428, activation='relu'))
-model.add(Dense(28, activation='relu'))
-model.add(Dense(1, activation='tanh')) # !!!!! логичней tanh так так есть положительные и отрицательные значения
-opt = tf.keras.optimizers.Adam(learning_rate=1e-5)
-model.compile(loss='mse', optimizer=opt, metrics=['mae'])
-# model.compile(loss='mae', optimizer=opt, metrics=['mae'])
-model.fit(X, YHead, batch_size=512, epochs=320, validation_split=0.2, verbose=2)
-
-# def modelTrain(Y):
-#  model = Sequential()
-#  model.add(Dense(228, input_dim=228, activation='relu'))
-#  model.add(Dense(30, activation='relu'))
-#  model.add(Dropout(0.5))
-#  model.add(Dense(2, activation='softmax'))
-#  opt = tf.keras.optimizers.RMSprop(learning_rate=1e-5)
-#  model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['acc'])
-#  model.fit(X, Y, batch_size=32, epochs=20, validation_split=0.2, verbose=2)
-#  return model
+# X = getFeaturesHead()
+# Y = getwinHeadTarget()
+# print(X.shape)
+# print(Y.shape)
 #
-# model = modelTrain(YHead)
-# model.save('modelHead.h5')
-# model = modelTrain(YMiddle)
-# model.save('modelMiddle.h5')
-# model = modelTrain(YTail)
-# model.save('modelTail.h5')
+# model = modelTrain(X,Y)
+# model.save('modelHeadGen1.h5')
+#
+# X = getFeaturesMiddle()
+# Y = getwinMiddleTarget()
+# print(X.shape)
+# print(Y.shape)
+#
+# model = modelTrain(X,Y)
+# model.save('modelMiddleGen1.h5')
+
+X = getFeaturesTail()
+Y = getwinTailTarget()
+print(X.shape)
+print(Y.shape)
+
+model = modelTrain(X,Y)
+model.save('modelTailGen1.h5')
