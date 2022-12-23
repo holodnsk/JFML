269a078601421dcbb94a8fbb56581db050370ad9
holodnsk <holodnsk@gmail.com>
2022-12-23 11:09:46 +0700

    drop unused

--- JFDL.py
+++ JFDL.py
@@ -39,31 +39,13 @@ def getFeaturesHead():
  return df_full.astype(bool)
 
 
-def modelTrain(X,Y):
- model = Sequential()
- model.add(Dense(165, input_dim=165, activation='relu'))
- model.add(Dense(428, activation='relu'))
- model.add(Dense(1028, activation='relu'))
- # model.add(Dense(4028, activation='relu'))
- # model.add(Dense(1028, activation='relu'))
- model.add(Dense(428, activation='relu'))
- model.add(Dense(100, activation='relu'))
- # model.add(Dense(428, activation='relu'))
- model.add(Dense(28, activation='relu'))
- model.add(Dense(1, activation='tanh'))
- opt = tf.keras.optimizers.Adam(learning_rate=1e-5)
- model.compile(loss='mse', optimizer=opt, metrics=['mae'])
- model.fit(X, Y, batch_size=2000, epochs=1, validation_split=0.2, callbacks=[csv_logger])
- return model
-
-
 model = Sequential()
 model.add(Dense(165, input_dim=165, activation='relu'))
 model.add(Dense(428, activation='relu'))
-# model.add(Dense(1028, activation='relu'))
+model.add(Dense(1028, activation='relu'))
 # model.add(Dense(4028, activation='relu'))
 # model.add(Dense(1028, activation='relu'))
-# model.add(Dense(428, activation='relu'))
+model.add(Dense(428, activation='relu'))
 model.add(Dense(100, activation='relu'))
 # model.add(Dense(428, activation='relu'))
 model.add(Dense(28, activation='relu'))
@@ -71,31 +53,35 @@ model.add(Dense(1, activation='tanh'))
 opt = tf.keras.optimizers.Adam(learning_rate=1e-5)
 model.compile(loss='mse', optimizer=opt, metrics=['mae'])
 
-X = getFeaturesHead()
-Y = getwinHeadTarget()
+# model = keras.models.load_model('modelTailGen1.h5')
 
-for n in range(0,2):
- xstart = 0
- xend = 10000000
- delta = 13000000
+X = getFeaturesTail()
+Y = getwinTailTarget()
+
+for n in range(0,10):
+ # xstart = 0
+ # xend = 130832450
+ # delta = 10000000
  print(X.shape)
  print(Y.shape)
- x_part = X[xstart:xend]
- y_part = Y[xstart:xend]
- print(str(x_part.shape[0]))
- while x_part.shape[0]>=2000:
-  print(str(x_part.shape[0]))
-  print(str(xstart),str(xend))
-  print(x_part.shape)
-  print(x_part.shape)
-  model.fit(x_part, y_part, batch_size=2000, epochs=1, validation_split=0.2, callbacks=[csv_logger])
-  xstart=xstart+delta
-  xend=xend+delta
-  x_part = X[xstart:xend]
-  y_part = Y[xstart:xend]
+ # x_part = X[xstart:xend]
+ # y_part = Y[xstart:xend]
+ # print(str(x_part.shape[0]))
+ while True:
+  # print(str(x_part.shape[0]))
+  # print(str(xstart),str(xend))
+  # print(x_part.shape)
+  # print(x_part.shape)
+  model.fit(X, Y, batch_size=20000, epochs=5, validation_split=0.2, callbacks=[csv_logger])
+  model.save(str(n)+'modelTailGen1.h5')
+  K.clear_session()
+  # xstart=xstart+delta
+  # xend=xend+delta
+  # x_part = X[xstart:xend]
+  # y_part = Y[xstart:xend]
 
 
-model.save('modelHeadGen1.h5')
+model.save('modelTailGen1.h5')
 
 # X = getFeaturesMiddle()
 # Y = getwinMiddleTarget()
